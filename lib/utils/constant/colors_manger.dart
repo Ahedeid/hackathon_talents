492d93431e7b9d2c3c4cc18e8e12350d9ebde4b8
Ahed-VICTUS <ahedeid@hotmail.com>
2023-03-13 16:32:06 +0200

    first commit

--- lib/utils/constant/colors_manger.dart
+++ lib/utils/constant/colors_manger.dart
@@ -1,10 +1,8 @@
 
 
 class AppColor{
-static const int primaryTextColor = 0xffFFFFFF;
-static const int floatingColor = 0xFF252525;
-static const int tealColor = 0xFF21AA93;
-static const int grayColor = 0xFFBDBDBD;
+static const int primaryTextColor = 0xFF151617F5;
+static const int primaryButtonColor = 0xFF4375FF;
 static const int backGroundSearchIconColor = 0xFF3B3B3B;
 static const int backGroundTextFieldColor = 0xFF3B3B3B;
 static const int linearGradientColorA = 0xFF21AA93;
